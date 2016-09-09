decc2af4b8ebd1c5b527eb5433239e1b82af0164
tpstar <hanlee12@gmail.com>
2016-09-09 17:47:06 +0000

    done

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,8 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(label, artists)
+    select_tag "artist", options_from_collection_for_select(artists, "id", "name")
+  end
 end
